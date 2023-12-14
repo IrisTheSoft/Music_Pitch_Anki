e610734563d29f5b7a05c7ad75f8079d08eae31e
Iridiscencia Álvarez <airidiscencia4@proton.me>
2023-12-14 11:09:41 -0300

    Templated clefs and signatures

---
+++ generate.py
@@ -0,0 +1,37 @@
+import dataclasses as DC
+import typing as TP
+
+import jinja2 as JJ
+
+
+@DC.dataclass
+class Clef:
+    name: str
+    sharps: TP.List[int]
+    flats: TP.List[int]
+
+
+treble_clef = Clef("G", [-4, -1, -5, -2, 1, -3, 0], [0, -3, 1, -2, 2, -1, 3])
+bass_clef = Clef("F", [-2, 1, -3, 0, 3, -1, 2], [2, -1, 3, 0, 4, 1, 5])
+
+
+with open("media.svg.jinja") as media_template_file:
+    media_template = JJ.Template(media_template_file.read())
+
+
+def generate_media(file_name, clef, signature_count):
+    if signature_count > 0:
+        signature_kind = "sharp"
+        signature = clef.sharps[:signature_count]
+    elif signature_count < 0:
+        signature_kind = "flat"
+        signature = clef.flats[:-signature_count]
+    else:
+        signature_kind = "empty"
+        signature = []
+    with open(f"output/{file_name}.svg", "w") as file:
+        file.write(media_template.render({
+            "clef": clef.name,
+            "signature_kind": signature_kind,
+            "signature": signature,
+            "enumerate": enumerate}))
