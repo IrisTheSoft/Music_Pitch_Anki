54b986d6e05506813c42bd5327ebd59a18b5b904
Iridiscencia Álvarez <airidiscencia4@proton.me>
2023-12-14 20:02:00 -0300

    Added answer generator

--- generate.py
+++ generate.py
@@ -11,34 +11,60 @@ media_path = PTH.Path("output")
 @DC.dataclass
 class Clef:
     name: str
+    offset: int
     sharps: TP.List[int]
     flats: TP.List[int]
 
 
-treble_clef = Clef("G", [-4, -1, -5, -2, 1, -3, 0], [0, -3, 1, -2, 2, -1, 3])
-bass_clef = Clef("F", [-2, 1, -3, 0, 3, -1, 2], [2, -1, 3, 0, 4, 1, 5])
+treble_clef = Clef("G", 6, [-4, -1, -5, -2, 1, -3, 0], [0, -3, 1, -2, 2, -1, 3])
+bass_clef = Clef("F", -6, [-2, 1, -3, 0, 3, -1, 2], [2, -1, 3, 0, 4, 1, 5])
+
+note_names = ["do", "re", "mi", "fa", "sol", "la", "si"]
 
 
 with open("media.svg.jinja") as media_template_file:
     media_template = JJ.Template(media_template_file.read())
 
 
-def generate_media(file_name, clef, signature_count, note, accidental):
+def split_signature(clef, signature_count):
+    if signature_count > 0:
+        return clef.sharps[:signature_count]
+    elif signature_count < 0:
+        return clef.flats[:-signature_count]
+    else:
+        return []
+
+
+def generate_answer(clef, signature_count, note_position, accidental):
+    note = clef.offset - note_position
+    signature = [(clef.offset - signature_note) % 7 for signature_note
+                 in split_signature(clef, signature_count)]
+    if note % 7 not in signature:
+        semitone = {"flat": " b", "sharp": " #", None: ""}[accidental]
+    elif accidental == "natural":
+        semitone = ""
+    elif accidental == "flat" or signature_count < 0:
+        semitone = " b"
+    elif accidental == "sharp" or signature_count > 0:
+        semitone = " #"
+    return f"{note_names[note % 7].title()}{semitone} {4 + note // 7}"
+
+
+def generate_media(file_name, clef, signature_count, note_position, accidental):
     if signature_count > 0:
         signature_kind = "sharp"
-        signature = clef.sharps[:signature_count]
     elif signature_count < 0:
         signature_kind = "flat"
-        signature = clef.flats[:-signature_count]
     else:
         signature_kind = "empty"
-        signature = []
+    signature = split_signature(clef, signature_count)
+    print(generate_answer(clef, signature_count, note_position, accidental))
     with open(media_path/f"{file_name}.svg", "w") as file:
         file.write(media_template.render({
             "clef": clef.name,
             "signature_kind": signature_kind,
             "signature": signature,
-            "note": note,
+            "note": note_position,
             "accidental": accidental,
             "enumerate": enumerate,
             "list": list,
